fb39eef1e770aaf4c339d1100ff571f13b74280f
baileyRichardson <85188870+baileyRichardson@users.noreply.github.com>
2021-09-30 14:39:23 -0400

    adding API for accountSettings

--- accountSettings.py
+++ accountSettings.py
@@ -3,3 +3,24 @@ For account settings.
 
 Bailey Richardson
 """
+
+def get_account_preferences(user: User) -> List[accountPreferences]:
+    """
+    This method returns the account preferences for a user.
+    :param self: the User object for whom to retrieve the account preferences
+    :return: a list of account preference objects or an empty list if the user has no account preferences
+    """
+    pass
+
+
+def get_notification_preferences(user: User) -> List[notificationPreferences]:
+    """
+    This method returns the notification preferences for a user.
+    :param user: the User object for whom you want to get notification preferences
+    :return: a list of notification preference objects or an empty list if the user has no notification preferences
+    """
+    pass
+
+
+def hello() -> str:
+    return "Hello from component accountSettings"
