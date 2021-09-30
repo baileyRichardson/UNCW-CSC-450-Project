08b46f5764bf92f79efde0c25090378b0203b3a1
Tan-N <tdn5547@uncw.edu>
2021-09-29 23:58:20 -0400

    Milestone 4 Class Skeleton - Tan
    Includes 3 class skeleton, added methods to components also.

---
+++ steamGame.py
@@ -0,0 +1,11 @@
+class steamGame:
+
+    def __init__(self, gameid):
+        self.gameid = gameid
+
+    #And other attributes
+    gameName: str = ""
+    gamePrice: str = ""
+    gameTags: list[str] = []
+
+
--- userManger.py
+++ userManger.py
@@ -1,1 +1,19 @@
-#Initial file for User Manager Component - Tan
+#userManager Component - Tan Ngo
+
+def hello ():
+    return "Hello from Component steamSetting"
+
+def get_user(username: str) -> User:
+    """
+    This function just returns the current user based on who is logged in.
+    :return: The userid based on the username, raises exception if user does not exist.
+    """
+    pass
+
+def get_email(user: User) -> str:
+    """
+    this function returns the email of the associated user.
+    :param userid: The id of the user.
+    :return: The email of the user associated.
+    """
+    pass
