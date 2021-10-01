8bdb44edf4eedc917ff6c0e49573e7c407b28133
Adan Narvaez Munguia <adan.narvaez.munguia@gmail.com>
2021-09-30 21:17:51 -0400

    Reports component method signature.

--- Report.py
+++ Report.py
@@ -1,1 +1,27 @@
-#Adan Narvaez Munguia
+# Adan Narvaez Munguia
+def get_user_id(user) -> int:
+    """
+    This method returns the userid for a specified user.
+    :param user: the User you want to get the ID for.
+    :return: The user's ID number or 0 if the user does not exist.
+    """
+    user = 0
+    return 0
+
+
+def report_data(user) -> list[str]:
+    """
+    This method passes a report to the notification component.
+    :param user: the User the report is for.
+    :return: A report as an array of JSON strings.
+    """
+    user = 0
+    return ["report"]
+
+
+def hello() -> str:
+    """
+    This method says hello :)
+    :return: hi
+    """
+    return "Hello from Component Report"
