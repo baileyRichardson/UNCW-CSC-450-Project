5ed1ef1733ffff5491fdff59fce411b554720369
baileyRichardson <85188870+baileyRichardson@users.noreply.github.com>
2021-10-14 14:57:37 -0400

    html templates for front end

--- app.py
+++ app.py
@@ -5,16 +5,26 @@ import steamSetting
 import userManger
 import accountSettings
 import Report
+from flask import *
+import pyrebase
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():  # put application's code here
-    #return hello()
-    return '<h3>' + Notifications.hello() + '<h3><h3>' + Playtime.hello() + '<h3><h3>' + steamSetting.hello() +\
-           '<h3><h3>' + userManger.hello() + '<h3><h3>' + accountSettings.hello() + '<h3>''<h3>' + Report.hello() + '<h3>'
+@app.route('/', methods = ["get", "post"])
+
+# def hello_world():  # put application's code here
+#     #return hello()
+#     return '<h3>' + Notifications.hello() + '<h3><h3>' + Playtime.hello() + '<h3><h3>' + steamSetting.hello() +\
+#            '<h3><h3>' + userManger.hello() + '<h3><h3>' + accountSettings.hello() + '<h3>''<h3>' + Report.hello() + '<h3>'
 
 # call hello method from all six here
+
+def login():
+    if request.method == "post":
+        email = request.form["name"]
+        password = request.form["pass"]
+
+    return render_template('loginPage.html')
 
 if __name__ == '__main__':
     app.run()
