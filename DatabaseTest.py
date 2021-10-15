e07a2137ce95815658cafeec933e43ba6bd54271
MattJarr <matthewjar2000@gmail.com>
2021-10-15 13:39:22 -0400

    Fixed function naming conventions

--- DatabaseTest.py
+++ DatabaseTest.py
@@ -4,53 +4,53 @@ import Database
 
 def test(username, steamName):
     # delete the old test user
-    Database.deleteUser(username)
+    Database.delete_user(username)
     # creat the new test user
-    Database.createUser(username, "test@gmail.com")
+    Database.create_user(username, "test@gmail.com")
     # add steam accounts to the new user
-    Database.addSteamAccount(username, 1)
-    Database.addSteamAccount(username, steamName)
+    Database.add_steam_account(username, 1)
+    Database.add_steam_account(username, steamName)
     # get reference to a steam account from the account ID
-    print(Database.getSteamAccount(username, steamName))
+    print(Database.get_steam_account(username, steamName))
     # remove a steam account
-    Database.deleteSteamAccount(username, 1)
+    Database.delete_steam_account(username, 1)
     # toggle auto tracking
-    Database.toggleAutoTrack(username, steamName)
-    Database.toggleAutoTrack(username, steamName)
+    Database.toggle_auto_track(username, steamName)
+    Database.toggle_auto_track(username, steamName)
     # add games that are automatically added to the tracking
-    Database.addOwnedGame("GTA V", username, steamName, True)
-    Database.addOwnedGame("Portal 2", username, steamName, True)
-    Database.addOwnedGame("Minecraft", username, steamName, True)
+    Database.add_owned_game("GTA V", username, steamName, True)
+    Database.add_owned_game("Portal 2", username, steamName, True)
+    Database.add_owned_game("Minecraft", username, steamName, True)
     # add games that are not automatically added to the tracking
-    Database.addOwnedGame("Halo", username, steamName, False)
-    Database.addOwnedGame("Overwatch", username, steamName, False)
+    Database.add_owned_game("Halo", username, steamName, False)
+    Database.add_owned_game("Overwatch", username, steamName, False)
     # add a game to be tracked
-    Database.addTrackedGame("Halo", username, steamName)
+    Database.add_tracked_game("Halo", username, steamName)
     # remove a game altogether
-    Database.removeOwnedGame("Minecraft", username, steamName)
+    Database.remove_owned_game("Minecraft", username, steamName)
     # remove a game from being tracked
-    Database.removeTrackedGame("GTA V", username, steamName)
+    Database.remove_tracked_game("GTA V", username, steamName)
     # set how long a period of time you want to impose a limit over (week, month, day)
-    Database.setLimitDuration(username, steamName, "month")
+    Database.set_limit_duration(username, steamName, "month")
     # print how long a period of time you want to impose a limit over
-    print(Database.getLimitDuration(username, steamName))
+    print(Database.get_limit_duration(username, steamName))
     # set how long you want to be allowed to play within a given time
-    Database.setPlaytimeLimit(username, steamName, 10.2)
+    Database.set_playtime_limit(username, steamName, 10.2)
     # print how long you want to be allowed to play within a given time
-    print(Database.getPlaytimeLimit(username, steamName))
+    print(Database.get_playtime_limit(username, steamName))
     # check if a game is being tracked
-    print(Database.checkforTracked(username, steamName, "Portal 2"))
-    print(Database.checkforTracked(username, steamName, "GTA V"))
+    print(Database.check_for_tracked(username, steamName, "Portal 2"))
+    print(Database.check_for_tracked(username, steamName, "GTA V"))
     # check if a game is owned
-    Database.checkforOwned(username, steamName, "Portal 2")
+    Database.check_for_owned(username, steamName, "Portal 2")
     # update the play time on a game that's tracked, automatically updating the total time
-    Database.updatePlayTime(username, steamName, "Portal 2", 2.4)
+    Database.update_playtime(username, steamName, "Portal 2", 2.4)
     # start watching a game
-    Database.addWatchGame(username, steamName, "Cyberpunk 2077", 30.00)
-    Database.addWatchGame(username, steamName, "Metroid Dread", 30.00)
+    Database.add_watch_game(username, steamName, "Cyberpunk 2077", 30.00)
+    Database.add_watch_game(username, steamName, "Metroid Dread", 30.00)
     # remove game from watch list
-    Database.removeWatchGame(username, steamName, "Metroid Dread")
-    Database.updateWatchGame(username, steamName, "Cyberpunk 2077", 40.00)
+    Database.remove_watch_game(username, steamName, "Metroid Dread")
+    Database.update_watch_game(username, steamName, "Cyberpunk 2077", 40.00)
     # print the total playtime over all games
-    print(Database.getTotalPlaytime(username, steamName))
+    print(Database.get_total_playtime(username, steamName))
     return username
