1732c6d6d09b0a97a4955ef884639dff92780f47
adanmunguia <87458470+adanmunguia@users.noreply.github.com>
2021-10-22 14:29:18 -0400

    Merging changes from Dr. Layman into this branch.

--- Report.py
+++ Report.py
@@ -1,5 +1,13 @@
 # Adan Narvaez Munguia
+from typing import List
 from Playtime import Playtime
+from User import User
+
+
+class ReportException(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
 
 
 class Report():
@@ -14,14 +22,23 @@ class Report():
         for account in user_list:
             self.steam_accounts.append(Playtime(account))
 
+
+    def report_data(user) -> List[str]:
+        """
+        This method passes a report to the notification component.
+        :param user: the User the report is for.
+        :return: A report as an array of JSON strings.
+        """
+        user = 0
+        return ["report"]
+
     def list_steam_accounts(self) -> list:
         """
         Lists the display names of all of a user's steam accounts.
         :return: The display names as a list of strings.
         """
         if len(self.steam_accounts) == 0:
-            print("No Steam Accounts are linked!")
-            return []
+            raise ReportException(message="No Steam Accounts Found!")
         display_name_list = []
         for account in self.steam_accounts:
             display_name_list.append(account.get_display_name())
