efd3797e7e56dfbb23cec7a78b000b004a2c9899
Lucas Layman <laymanl@uncw.edu>
2021-10-22 10:06:43 -0400

    Add unit test demo + updated requirements.txt for test libraries

--- Report.py
+++ Report.py
@@ -1,15 +1,27 @@
 # Adan Narvaez Munguia
-def get_user_id(user) -> int:
+from typing import List
+
+from User import User
+
+
+class ReportException(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+
+def get_user_id(user: User) -> int:
     """
     This method returns the userid for a specified user.
     :param user: the User you want to get the ID for.
     :return: The user's ID number or 0 if the user does not exist.
     """
-    user = 0
-    return 0
+    if user.username == "jimbob":
+        return 0
+    raise ReportException(message="User not found")
 
 
-def report_data(user) -> list[str]:
+def report_data(user) -> List[str]:
     """
     This method passes a report to the notification component.
     :param user: the User the report is for.
