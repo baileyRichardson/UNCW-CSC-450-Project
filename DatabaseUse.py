e17677d37d5891e778a79156e44cd5c37a6d679c
MattJarr <matthewjar2000@gmail.com>
2021-11-05 14:38:29 -0400

    Steam account linking
    Steam account can be linked to user, stored on database. Notifications page now interacts both ways with database

--- DatabaseUse.py
+++ DatabaseUse.py
@@ -21,5 +21,27 @@ def update_steam_account_page(userID: str, steamID: int, auto: str, remove: str,
         return 2
 
 
+def add_steam_account(userID: str, steamID: int):
+    database.add_steam_account(userID, steamID)
+    return 1
+
+
+def update_notifications_page(userID: str, often: int):
+    database.update_notif_time(userID, often)
+    return 1
+
+
+def interpret_notification_time(userID: str):
+    often = database.get_notif_time(userID)
+    if often == 1:
+        return "Every day"
+    elif often == 2:
+        return "Once a week"
+    elif often == 3:
+        return "Every two weeks"
+    elif often == 4:
+        return "Once a month"
+    else:
+        return "Error - invalid time stored"
 
 
