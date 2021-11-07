f3ceb02d892856e59938566031f17a5574e60c20
baileyRichardson <85188870+baileyRichardson@users.noreply.github.com>
2021-11-07 13:01:57 -0500

    Pulls data from Report and sends it via email with proper formatting

--- Mail.py
+++ Mail.py
@@ -7,31 +7,46 @@
 #     userName: str = ""
 
 import smtplib
-import time
 from Report import Report
-from email.message import EmailMessage
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 def send_email(email : str, frequency : int):
-    user_id = "10000"
-    user_report = Report(user_id)
 
-    msg = EmailMessage()
-    msg["Subject"] = "This is a test"
+    msg = MIMEMultipart("alternative")
+    msg["Subject"] = "Steam Monitor Report for " + email
     msg["from"] = "noreplySteamMonitor@gmail.com"
     msg["to"] = email
 
-    report_text = str(user_report.generate_report_text())
+    user_id = "10000"
+    user_report = Report(user_id)
+    user_report_text = user_report.generate_report_text()
 
+    email_body = """\
+    <html>
+        <body>
+        """
+    for account in user_report_text:
+        email_body = email_body + "<p> Hello, " + "here is a report for the Steam accounts linked to " + email + "</p>\n"
+        email_body = email_body + "<p> Steam account: " +  account[0] + "</p>" + "\n" + "<table>"
+        email_body = email_body + "<tr><th>Game</th><th>Playtime</th></tr>" + "\n"
+        for game in account[1]:
+            email_body = email_body + "<tr><td>" + game + "</td>" + "<td>" + str(account[1][game][0]) + "</td>" + "</tr>" + "\n"
+        email_body = email_body + "</table>" + """</body>
+        </html>
+        """
 
-    print(report_text)
-    msg.set_content(report_text)
+    html = email_body
 
-    # with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
-    #
-    #     smtp.login("noreplySteamMonitor@gmail.com", "UNCWcsc450")
-    #
-    #     smtp.send_message(msg)
-    #     time.sleep(frequency * 60)
+    html_msg = MIMEText(html, "html")
+
+    msg.attach(html_msg)
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+
+        smtp.login("noreplySteamMonitor@gmail.com", "UNCWcsc450")
+
+        smtp.send_message(msg)
 
 send_email("baileyr0826@gmail.com", 1)
 
