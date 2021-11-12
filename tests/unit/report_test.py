7e580087a9fa874b11737837fd28d960b88d25e9
Adan Narvaez Munguia <adan.narvaez.munguia@gmail.com>
2021-11-11 22:37:58 -0500

    CSS and bug suppression.

--- tests/unit/report_test.py
+++ tests/unit/report_test.py
@@ -23,7 +23,7 @@ def test_instantiate_report_id_not_found():
 
 def test_generate_report():
     # "10000" corresponds to a test user. user_id is usually an email.
-    user_id = "10000"
+    user_id = "aan7056@uncwedu"
     # Instantiating a report object
     test_report = Report(user_id)
     # Connecting to Steam and grabbing the random account.
