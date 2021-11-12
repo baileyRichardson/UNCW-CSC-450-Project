96c5b01e3d11d6a10889aedd867e323db3d609bd
MattJarr <matthewjar2000@gmail.com>
2021-11-11 20:04:15 -0500

    Update databaseuse_test.py

--- tests/unit/databaseuse_test.py
+++ tests/unit/databaseuse_test.py
@@ -39,14 +39,6 @@ def test_toggle_auto():
     assert DatabaseUse.update_steam_account_page(userID, steamID, auto, remove, limit) == 2
 
 
-def test_add_steam_account():
-    # a test account
-    userID = "test@gmail"
-    #an invalid steam account ID
-    steamID = "asdfghj"
-    assert DatabaseUse.add_steam_account(userID, steamID) is False
-
-
 def test_add_to_watchlist():
     # a test account
     userID = "test@gmail"
@@ -62,6 +54,5 @@ def test_add_to_watchlist():
 def run_all():
     test_update_notifications_page()
     test_toggle_auto()
-    #test_add_steam_account()
     test_remove_steam_account()
     test_add_to_watchlist()
