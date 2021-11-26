4fae1eee42c79608c5108ce5b8b903a82e32d2c4
Tan-N <tdn5547@uncw.edu>
2021-11-26 15:49:52 -0500

    Adapting to main
    Changing a few unit test to comply with main.py name change

--- tests/unit/authorization_test.py
+++ tests/unit/authorization_test.py
@@ -2,35 +2,35 @@
 The test is functioning, but not with intended results at the time being"""
 
 import pytest
-import app
+import main
 
 def test_authorize_dashboard_nologin():
     with pytest.raises(expected_exception=AttributeError):
-        app.render_template("dashboard.html")
+        main.render_template("dashboard.html")
 
 def test_authorize_reports_nologin():
     with pytest.raises(expected_exception=AttributeError):
-        app.render_template("reports.html")
+        main.render_template("reports.html")
 
 def test_authorize_settings_nologin():
     with pytest.raises(expected_exception=AttributeError):
-        app.render_template("settings.html")
+        main.render_template("settings.html")
 
 def test_authorize_settingsNotif_nologin():
     with pytest.raises(expected_exception=AttributeError):
-        app.render_template("settingNotifications.html")
+        main.render_template("settingNotifications.html")
 
 def test_authorize_settingsWatch_nologin():
     with pytest.raises(expected_exception=AttributeError):
-        app.render_template("settingWatchList.html")
+        main.render_template("settingWatchList.html")
 
 def test_authorize_settingsPlaytime_nologin():
     with pytest.raises(expected_exception=AttributeError):
-        app.render_template("settingPlaytimeTracking.html")
+        main.render_template("settingPlaytimeTracking.html")
 
 def test_authorize_settingsSteam_nologin():
     with pytest.raises(expected_exception=AttributeError):
-        app.render_template("settingSteamAccount.html")
+        main.render_template("settingSteamAccount.html")
 
 
 
