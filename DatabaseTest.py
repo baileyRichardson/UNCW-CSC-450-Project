5cd949e681999536f2b3c5cff73c5c31ce64bed7
adanmunguia <87458470+adanmunguia@users.noreply.github.com>
2021-11-26 17:04:55 -0500

    Merged Database edits.

--- DatabaseTest.py
+++ DatabaseTest.py
@@ -2,7 +2,7 @@ import pyrebase
 import Database
 
 
-def test(username, steamName):
+def test(username="test@gmailcom", steamName=76561198079743061):
     # delete the old test user
     Database.delete_user(username)
     # creat the new test user
@@ -11,9 +11,7 @@ def test(username, steamName):
     print(Database.get_user(username))
     print(Database.get_email(username))
     # add steam accounts to the new user
-    Database.add_steam_account(username, 1)
     Database.add_steam_account(username, steamName)
-    Database.add_steam_account(username, 67890)
     # get reference to a steam account from the account ID
     print(Database.get_steam_account(username, steamName))
     # remove a steam account
@@ -38,28 +36,28 @@ def test(username, steamName):
     # print how long a period of time you want to impose a limit over
     print(Database.get_limit_duration(username, steamName))
     # set how long you want to be allowed to play within a given time
-    Database.set_playtime_limit(username, steamName, 10.2)
+    Database.set_playtime_limit(username, steamName, 100)
     # print how long you want to be allowed to play within a given time
     print(Database.get_playtime_limit(username, steamName))
     # check if a game is being tracked
-    print(Database.check_for_tracked(username, steamName, "Portal 2"))
-    print(Database.check_for_tracked(username, steamName, "GTA V"))
+    print(Database.game_tracked("Portal 2", username, steamName))
+    print(Database.game_tracked("GTA V", username, steamName))
     # update the play time on a game that's tracked, automatically updating the total time
-    Database.update_playtime(username, steamName, "Portal 2", 2.4)
+    Database.update_playtime("Portal 2", username, steamName, 120)
     # remove a game from being tracked
     Database.remove_tracked_game("Portal 2", username, steamName)
     # add a game to be tracked
     Database.add_tracked_game("Portal 2", username, steamName)
+    print(Database.update_playtime("Portal 2", username, steamName, 180))
+    Database.get_daily_playtime("Portal 2", username, steamName)
     # start watching a game
     Database.add_watch_game(username, steamName, "Cyberpunk 2077", 30.00)
     Database.add_watch_game(username, steamName, "Metroid Dread", 30.00)
     # remove game from watch list
     Database.remove_watch_game(username, steamName, "Metroid Dread")
     Database.update_watch_game(username, steamName, "Cyberpunk 2077", 40.00)
-    # print the total playtime over all games
-    print(Database.get_total_playtime(username, steamName))
     # print list of watched games
-    print("Watched games:",Database.list_of_watched_games(username, steamName))
+    print("Watched games:", Database.list_of_watched_games(username, steamName))
     # print list of tracked games
     print(Database.list_of_tracked_games(username, steamName))
     # print list of playtimes
