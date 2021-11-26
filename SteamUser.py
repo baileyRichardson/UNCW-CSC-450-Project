e86fcbc0f842415377a18ebdb3d24d0ac3c0f2e8
adanmunguia <87458470+adanmunguia@users.noreply.github.com>
2021-11-26 11:09:56 -0500

    Fix for the datatables bug.

--- SteamUser.py
+++ SteamUser.py
@@ -1,5 +1,6 @@
 class SteamUser:
-    def __init__(self, steam_id: int, steam_name: str, app_ids: [int], names: [str], img_icons: [str], playtimes: [int], monthly_playtimes: [int], daily_playtimes: [int]):
+    def __init__(self, steam_id: int, steam_name: str, app_ids: [int], names: [str], img_icons: [str],
+                 playtimes: [int], monthly_playtimes: [int], daily_playtimes: [int]):
         self.__steam_id = steam_id
         self.__steam_name = steam_name
         self.__appid_array = app_ids
