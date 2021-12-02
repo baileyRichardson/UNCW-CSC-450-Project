ae38fc56038942f6fe9f6204d19811550bc7769b
baileyRichardson <85188870+baileyRichardson@users.noreply.github.com>
2021-12-01 23:52:53 -0500

    Displays watch price and store price on dashboard
    Probably the ugliest thing ever, but it works for the moment. I'll stay up tonight trying to clean it up

--- Subprocess.py
+++ Subprocess.py
@@ -20,24 +20,24 @@ def compare_with_steam_store(user_email : str):
     for account in account_list:
         user_watched_games = Database.list_of_watched_games(user_email, account.get_steam_id())
         watched_by_steam_account.append(user_watched_games)
-        print("Watched games and price for ", account, ": ", user_watched_games)
+        # print("Watched games and price for ", account, ": ", user_watched_games)
 
         for game in user_watched_games:
             watch_game_price = Database.get_watch_game_price(user_email, account.get_steam_id(), game[0])
-            print(game[0], " watch price --> ", watch_game_price)
+            # print(game[0], " watch price --> ", watch_game_price)
             watched_game_ids = Database.get_watch_game_ID(user_email, account.get_steam_id(), game[0])
             steam_store_game = SteamStore.get_app_details(watched_game_ids)
             steam_store_price = float(steam_store_game.get('price'))
             steam_store_price /= 100
             steam_store_price = steam_store_price.__round__(1)
-            print(game[0], "store price --> ", steam_store_price)
+            # print(game[0], "store price --> ", steam_store_price)
 
             if watch_game_price >= steam_store_price:
                 watched_equals_store[game[0]] = True
             else:
                 watched_equals_store[game[0]] = False
 
-    print(watched_equals_store)
+    # print(watched_equals_store)
     return watched_equals_store
 
 
@@ -47,23 +47,22 @@ def watch_vs_store_price(user_email : str):
     watched_by_steam_account = []
     account: SteamUser
     watched_vs_store = {}
-    # watched_vs_store = []
     for account in account_list:
         user_watched_games = Database.list_of_watched_games(user_email, account.get_steam_id())
         watched_by_steam_account.append(user_watched_games)
-        print("Watched games and price for ", account, ": ", user_watched_games)
+        # print("Watched games and price for ", account, ": ", user_watched_games)
 
         for game in user_watched_games:
             watch_game_price = Database.get_watch_game_price(user_email, account.get_steam_id(), game[0])
-            print(game[0], " watch price --> ", watch_game_price)
+            # print(game[0], " watch price --> ", watch_game_price)
             watched_game_ids = Database.get_watch_game_ID(user_email, account.get_steam_id(), game[0])
             steam_store_game = SteamStore.get_app_details(watched_game_ids)
             steam_store_price = float(steam_store_game.get('price'))
             steam_store_price /= 100
             steam_store_price = steam_store_price.__round__(1)
-            print(game[0], "store price --> ", steam_store_price)
+            # print(game[0], "store price --> ", steam_store_price)
 
             watched_vs_store[game[0]] = [watch_game_price, steam_store_price]
             # watched_vs_store.append([game[0], watch_game_price, steam_store_price])
-    print(watched_vs_store)
+    # print(watched_vs_store)
     return watched_vs_store
